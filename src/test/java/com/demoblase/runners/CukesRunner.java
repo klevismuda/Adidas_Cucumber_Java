fae311bc72c8e992e34180f0819cc5657f29fab8
Klevis Muda <klevis.blue@gmail.com>
2021-07-15 19:12:32 +0200

    Final with report

--- src/test/java/com/demoblase/runners/CukesRunner.java
+++ src/test/java/com/demoblase/runners/CukesRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/adidas_report.html",
         features = "src/test/resources/features",
         glue = "com/demoblase/step_definitions",
         dryRun = false
